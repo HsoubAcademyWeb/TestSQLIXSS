271f370e556f4aabacb88c640a48e850512263ed
HsoubAcademyWeb <56909275+HsoubAcademyWeb@users.noreply.github.com>
2020-03-02 16:03:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
 				?>
 					<div class="alert alert-danger"><?php echo $_SESSION['error']?></div>
 				<?php
-					session_unset($_SESSION['error']);
+					session_unset();
 					}
 				?>
 				<div class="form-group">
